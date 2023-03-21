44a063a03b1e1ac5f29b0aecaec66a266e9e3ec0
Felix Vybíhal <vyb39100@spsol.cz>
2023-03-21 14:01:44 +0100

    jeej

--- main.py
+++ main.py
@@ -55,11 +55,37 @@ class Application(tk.Tk):
         self.columnRadio = tk.Radiobutton(self.fileFrame,text="data jsou ve sloupcích", variable = self.dataVar, value= "column")
         self.columnRadio.pack(anchor="w")
 
+        #graf
+        self.grafFrame = tk.LabelFrame(self,text ="Graf")
+        self.grafFrame.pack(padx=5,pady=5,fill= "x")
+
+        tk.Label(self.grafFrame, text="Titulek").grid(row=0,column=0)
+        self.titleEntry = MyEntry(self.grafFrame)
+        self.titleEntry.grid(row=0,column=1)
+
+        tk.Label(self.grafFrame, text="OsaX").grid(row=1,column=0)
+        self.xlabelEntry = MyEntry(self.grafFrame)
+        self.xlabelEntry.grid(row=1,column=1)
+
+        tk.Label(self.grafFrame, text="OsaY").grid(row=2,column=0)
+        self.ylabelEntry = MyEntry(self.grafFrame)
+        self.ylabelEntry.grid(row=2,column=1)
+
+        tk.Label(self.grafFrame, text = "Čára").grid(row=3,column=0)
+        self.lineVar = tk.StringVar(value= "-")
+        tk.OptionMenu(self.grafFrame,self.lineVar,"none", ":","-.","--","-").grid(row=3,column=1, sticky="w")
+
+        tk.Label(self.grafFrame, text = "Marker").grid(row=4,column=0)
+        self.markerVar = tk.StringVar(value= "o")
+        tk.OptionMenu(self.grafFrame,self.markerVar,"none", *tuple(".,o+PxX*1234<>v^")).grid(row=4,column=1, sticky="w")
+
+        #tlačítka
         self.plotBtn = tk.Button(self, text="steč", command=self.plot)
         self.plotBtn.pack(fill = "x")
 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
+
 
     def quit(self, event=None):
         super().quit()
@@ -76,8 +102,25 @@ class Application(tk.Tk):
 
                 x = [float(i.replace(",",".")) for i in x]
                 y = [float(i.replace(",",".")) for i in y]
+            elif self.dataVar.get()=="column":
+                x = []
+                y = []
+                while True:
+                    line = f.readline()
+                    if line == "":
+                        break
+                    if ";" not in line:
+                        continue
+                    x1 ,y1 = line.split(";")
+                    x.append(float(x1.replace(",",".")))
+                    y.append(float(y1.replace(",",".")))
+
+
         print(x,y)
-        py.plot(x,y)
+        py.plot(x,y, linestyle = self.lineVar.get())
+        py.title(self.titleEntry.value)
+        py.xlabel(self.xlabelEntry.value)
+        py.ylabel(self.ylabelEntry.value)
         py.show()
 
     
